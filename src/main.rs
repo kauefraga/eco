516992e1dd25c1206feed3d10ccd597acc4fc7a8
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-02-22 20:17:58 -0300

    feat: implement command echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+
+    /*
+     * Read the `remove` docstring.
+     * This `remove(0)` is the worst case,
+     * all the arguments are going to be
+     * shifted to the left, everytime.
+     */
+    args.remove(0);
+
+    let mut output = String::new();
+
+    for arg in args {
+        output.push_str(&format!("{arg} "));
+    }
+
+    println!("{}", output.trim());
 }
