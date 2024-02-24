74f619167eb13ac27376f04b6caeb5067dc0f8e8
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-02-23 23:26:05 -0300

    feat: optimize first element removing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
+use std::collections::VecDeque;
 use std::env;
 use std::process::exit;
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
+    let mut args: VecDeque<String> = env::args().collect();
 
-    /*
-     * Read the `remove` docstring.
-     * This `remove(0)` is the worst case,
-     * all the arguments are going to be
-     * shifted to the left, everytime.
-     */
-    args.remove(0);
+    args.pop_front();
 
     let is_help_needed = (args.len() == 1 && args[0] == "-h")
         || (args.len() == 1 && args[0] == "--help");
