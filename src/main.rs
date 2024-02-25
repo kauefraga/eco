b4cb32882b48c0de2345b52a3f41baf4c3fe74fe
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-02-25 13:38:46 -0300

    feat: use next() instead of manually counting arguments (more readable)

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,32 @@ use console::style;
 
 fn main() {
     let mut output = String::new();
-    let mut args_counter = 0;
 
-    for arg in env::args() {
-        // Skip the first arg (executable)
-        if args_counter == 0 {
-            args_counter += 1;
-            continue;
-        }
+    let mut args = env::args();
+    args.next(); // skip first argument (executable)
 
-        let is_help_needed = env::args().len() == 2
-            && (arg == "-h" || arg == "--help");
-        if is_help_needed {
-            println!("{}", format!(
-                "{}: eco-rs bom dia! {}",
-                style("Example").green(),
-                style("// bom dia!").dim()
-            ));
+    match args.next() {
+        Some(a) => {
+            let is_help_needed = env::args().len() == 2
+                && (a == "-h" || a == "--help");
+            if is_help_needed {
+                println!("{}", format!(
+                    "{}: eco-rs bom dia! {}",
+                    style("Example").green(),
+                    style("// bom dia!").dim()
+                ));
+                exit(0);
+            }
+
+            output.push_str(&format!("{a} "));
+        },
+        None => {
+            println!("");
             exit(0);
         }
+    }
 
+    while let Some(arg) = args.next() {
         if arg.to_lowercase().contains("rust") {
             arg.split(' ').for_each(|word| {
                 if word.to_lowercase().contains("rust") {
@@ -40,7 +46,6 @@ fn main() {
         }
 
         output.push_str(&format!("{arg} "));
-        args_counter += 1;
     }
 
     println!("{}", output.trim())
