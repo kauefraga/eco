6dc7c77e4326590c2ab93b778c89adb485a5f142
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-02-24 21:25:26 -0300

    feat: use console to display colored stuff

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,41 @@ use std::collections::VecDeque;
 use std::env;
 use std::process::exit;
 
+use console::style;
+
 fn main() {
     let mut args: VecDeque<String> = env::args().collect();
 
     args.pop_front();
 
-    let is_help_needed = (args.len() == 1 && args[0] == "-h")
-        || (args.len() == 1 && args[0] == "--help");
+    let is_help_needed = args.len() == 1
+        && (args[0] == "-h" || args[0] == "--help");
     if is_help_needed {
-        println!("Example: eco-rs Bom dia!");
+        println!("{}", format!(
+            "{}: eco-rs bom dia! {}",
+            style("Example").green(),
+            style("// bom dia!").dim()
+        ));
         exit(0);
     }
 
     let mut output = String::new();
 
     for arg in args {
+        if arg.to_lowercase().contains("rust") {
+            arg.split(' ').for_each(|word| {
+                if word.to_lowercase().contains("rust") {
+                    output.push_str(&format!(
+                        "{} ",
+                        style(word).red()
+                    ));
+                } else {
+                    output.push_str(&format!("{word} "));
+                }
+            });
+            continue;
+        }
+
         output.push_str(&format!("{arg} "));
     }
 
